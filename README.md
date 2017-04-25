38f2f91eb0389a42db6c0ef288f255eb5db1b9a3
yura <yura@nixsolutions.com>
2017-04-25 11:32:57 +0300

    all files

---
+++ README.md
@@ -0,0 +1,9 @@
+## captivaNavi
+
+### Downdload [...](dist/)
+
+### Change history
+#### Version 3
+  Only flash card. Changes in navi internal fs no need.
+  Copy file from archive into the card, insert card to Navi and restart Navi(Press Menu and Trip during 10 seconds)
+  
---
+++ src/patch script/patch1OneArea.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp LCN_Main.exe LCN_Main_oneArea.exe
+# com3 -> com5
+printf '\x35' | dd of=LCN_Main_oneArea.exe bs=1 seek=$((0xB4BFC-0x12000 + 6)) count=1 conv=notrunc 
+#path to Turbodog -> "Storage Card/Navi/Turbodog.exe"
+printf '\x53\x0\x74\x0\x6f\x0\x72\x0\x61\x0\x67\x0\x65\x0\x20\x0\x43\x0\x61\x0\x72\x0\x64\x0\x5c' | dd of=LCN_Main_oneArea.exe bs=1 seek=$((0xB3C26-0x12000)) conv=notrunc
+printf '\x53\x0\x74\x0\x6f\x0\x72\x0\x61\x0\x67\x0\x65\x0\x20\x0\x43\x0\x61\x0\x72\x0\x64\x0\x5c' | dd of=LCN_Main_oneArea.exe bs=1 seek=$((0xBA1C2-0x12000)) conv=notrunc
+
+
+# Don`t show "Caution screen"
+#cp LCN_Main_patch.exe LCN_Main_patch_1.exe
+printf '\x0\x10' | dd of=LCN_Main_patch.exe bs=1 seek=$((0x7B4F2)) conv=notrunc
+printf '\x0\x0\x0\x0' | dd of=LCN_Main_patch.exe bs=1 seek=$((0x7B398)) conv=notrunc
